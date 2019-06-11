48bd15a25673660297086c8f9c3b06854957157e
AllieWu <44093216+AllieWu@users.noreply.github.com>
2019-06-10 21:52:50 -0700

    Updated project

---
+++ World Gui/UnityUI/Assets/PageScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageScript : MonoBehaviour
+{
+    public Animator anim = null;  // Used to call Animator Controller triggers
+    bool flipReady = false;
+
+    void Flip()
+    {
+        anim.SetTrigger("Flip");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+}
