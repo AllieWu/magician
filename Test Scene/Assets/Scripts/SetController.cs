9f3c611623644eb56d91ebf86f80458cf43a4cbd
AllieWu <44093216+AllieWu@users.noreply.github.com>
2019-09-16 15:15:58 -0400

    updated

---
+++ Test Scene/Assets/Scripts/SetController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SetController : MonoBehaviour
+{
+    public Canvas setcontainer = null;  // the hierachy object that holds the sets
+    private static GameObject spellsPage = null;
+    private static GameObject invPage = null;
+    private static GameObject mapPage = null;
+    private static GameObject questsPage = null;
+    private GameObject[] pages = { };
+
+    private void Start()
+    {
+        spellsPage = setcontainer.transform.Find("DefaultSet").gameObject;
+        invPage = setcontainer.transform.Find("InvSet").gameObject;
+        questsPage = setcontainer.transform.Find("QuestSet").gameObject;
+        mapPage = setcontainer.transform.Find("MapSet").gameObject;
+
+        if (spellsPage != null)
+            Debug.Log("SpellsPage found");
+
+        spellsPage.SetActive(false);
+        invPage.SetActive(false);
+        mapPage.SetActive(false);
+        questsPage.SetActive(false);
+
+        pages = new GameObject[] { spellsPage, invPage, questsPage, mapPage };
+    }
+
+    // updates the current ui set displayed in the condensed ui
+    public void SetCurrentSet(int index)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            bool active = i == index ? true : false;
+            pages[i].SetActive(active);
+        }
+    }
+}
