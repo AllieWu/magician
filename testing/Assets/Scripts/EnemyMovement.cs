00fabaeafe854ed02544556898fc85566e6bbf81
Colin Kubisiak <colinz1999@gmail.com>
2019-10-09 14:56:43 -0400

    colinupdate

---
+++ testing/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+
+        body.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        body.velocity = new Vector2(-.05f, 0); // move at normal speed
+    }
+}
---
+++ testing/Assets/Scripts/physicsPlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movemnt : MonoBehaviour
+{
+
+    public float runSpeed;
+    public float moveLimiter;
+
+    float horizontal;
+    float vertical;
+
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+
+        body.freezeRotation = true;
+    }
+
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        if (horizontal == 0 && vertical == 0)
+        {
+            body.velocity = new Vector2(0, 0);
+        }
+        else
+        {
+            if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+                body.velocity = new Vector2((horizontal * runSpeed) * moveLimiter, (vertical * runSpeed) * moveLimiter); // move at less speed 
+            else // if not moving diagonally
+                body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed); // move at normal speed
+        }
+        
+
+    }
+}
