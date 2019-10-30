eea69287b58e188ea488658f7bf48a2ce0e24ea9
Robert Principato <42843903+rprince824@users.noreply.github.com>
2019-10-30 15:56:23 -0400

    Progress on spell creation ui

---
+++ Test Scene/Assets/Scripts/SpellCreationUI/SpellListButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpellListButton : MonoBehaviour
+{
+    public Button button;
+    public Text spellName;
+    public Image spellIcon;
+
+
+    void Start()
+    {
+        
+    }
+
+    public void Setup()
+    {
+
+    }
+}
