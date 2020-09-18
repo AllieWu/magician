192d1eaea173979c56adac455e92a7f412ec9443
Robert Principato <42843903+rprince824@users.noreply.github.com>
2020-09-18 17:19:39 -0400

    Added Laser spell

---
+++ Test Scene/Assets/Scripts/Spell Scripts/Attack/Laser/Laser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : Spell
+{
+    public Laser(float cd, float dm, float ct, bool ul, string ac, string sn, int id) : base(cd, dm, ct, ul, ac, sn, id)
+    {
+        //fbPrefab = GameObject.Find("Fireball");
+
+    }
+
+    public override void Cast()
+    {
+        
+    }
+}
---
+++ Test Scene/Assets/Scripts/Spell Scripts/Attack/Laser/LaserBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class LaserBehavior : MonoBehaviour
+{
+    private Laser ls;
+    private Vector2 direction;
+    private Rigidbody2D rb2d;
+
+    public void Start()
+    {
+        ls = GameObject.Find("Player").GetComponent<Laser>();
+    }
+
+    public void FixedUpdate()
+    {
+        direction = GameObject.Find("Player").GetComponent<PlayerController>().lookDirection;
+        
+    }
+
+    // What happens when the projectile hits another object with a collider
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Boundary")
+        {
+            //end the laser's path
+        }
+        else if (col.gameObject.tag == "Enemy")
+        {
+            col.gameObject.GetComponent<Unit>().DealDamage(ls.damage);
+            //end the laser's path
+        }
+    }
+}
